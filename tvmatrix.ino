2179ed6881947c7f7da20c8d1056cb88201fbdb4
viewport <88295234+v1ewport@users.noreply.github.com>
2023-01-13 01:47:43 -0300

    Update tvmatrix.ino

--- tvmatrix.ino
+++ tvmatrix.ino
@@ -1,27 +1,26 @@
 #include <TVout.h>
 
+struct COLUMN { int Y, LENGTH; };
+
 TVout TV;
 String CHARACTERS = "1234567890qwertyuiopasdfghjklzxcvbnm,.-_<>!#$%&/()=?¡";
-
-int COLUMN[18];
-int COL_LENGTH[18];
-
-void ERASE(int X, int Y) { TV.draw_line(X, Y, X+7, Y, 0); }
+COLUMN COLUMNS[18];
 
 void setup() {
+  randomSeed(analogRead(5));
   TV.start_render(_NTSC);
   TV.clear_screen(); 
-  for (int C=0; C<18; C++) { COL_LENGTH[C]=random(25, 80); }
-  for (int C=0; C<18; C++) { COLUMN[C] = random(-80, 0); }
+
+  for (int C=0; C<18; C++) { COLUMNS[C].Y = random(-80, 0); COLUMNS[C].LENGTH=random(25, 80); }
 }
 
 void loop() { 
   for (int C=0; C<18; C++) { 
-    if (COLUMN[C]-COL_LENGTH[C]>0) { TV.draw_line(C*7, COLUMN[C]-COL_LENGTH[C], (C*7)+7, COLUMN[C]-COL_LENGTH[C], 0); }
-    if (COLUMN[C]>0 && COLUMN[C]<TV.vert_res()) { TV.print_char(C*7, COLUMN[C], (char)CHARACTERS.charAt(random(0, strlen(CHARACTERS.c_str())))); }
-    COLUMN[C]++; 
+    if (COLUMNS[C].Y-COLUMNS[C].LENGTH>0) { TV.draw_line(C*7, COLUMNS[C].Y-COLUMNS[C].LENGTH, (C*7)+7, COLUMNS[C].Y-COLUMNS[C].LENGTH, 0); }
+    if (COLUMNS[C].Y>0 && COLUMNS[C].Y<TV.vert_res()) { TV.print_char(C*7, COLUMNS[C].Y, (char)CHARACTERS.charAt(random(0, strlen(CHARACTERS.c_str())))); }
+    COLUMNS[C].Y++; 
   }
   for (int C=0; C<18; C++) { 
-    if (COLUMN[C]>TV.vert_res()+COL_LENGTH[C]) { COLUMN[C]=random(-80, 0); COL_LENGTH[C]=random(25, 80); } 
+    if (COLUMNS[C].Y>TV.vert_res()+COLUMNS[C].LENGTH) { COLUMNS[C].Y = random(-80, 0); COLUMNS[C].LENGTH=random(25, 80); }
   }
 }
